01e385b5ff576bd298dc6cf8fd22ca1d3012bdba
Asim Riaz <asim.riaz@live.com>
2022-10-17 07:22:11 +0500

    Updates

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./screens/**/*.{tsx,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
+};
